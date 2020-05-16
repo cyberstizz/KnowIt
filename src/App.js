823dcf9ef592be9ce1357a9bbfb40c98c65801ee
Briandepalmas <Student_GA110@Student-GA110.home>
2020-05-16 13:11:07 -0400

    axios test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import './App.css';
-//brian
-//deji
-//
-function App() {
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    
+
+    axios.get("https://api.spoonacular.com/recipes/search?query=cheese&number=4&apiKey=27a02bbb5b48401f96bfda6a7d3e2545")
+    .then(res => {
+      const recipes = res.data.results;
+      console.log("recipes ---->", recipes)
+      
+      this.setState({data: recipes})
+
+    })
+   
+    .catch(error => {
+      console.log('there is an error', error)
+    })
+
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.data.map((recipe, id) => <h4 key={id}> {recipe.title} </h4>)}
+        </ul>
  
-  return (
-    <div className="App">
-     
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
-export default App;
