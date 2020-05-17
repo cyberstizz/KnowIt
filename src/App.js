3627d74a63d8849566d632b096ac88abdf595410
Briandepalmas <Student_GA110@Student-GA110.home>
2020-05-17 12:42:56 -0400

    removed axios test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import Modals from './components/Modals'
-import axios from 'axios'
-import Signup from './components/Signup'
 import DOMp from './components/assets/DOMp'
 export default class App extends Component {
 
