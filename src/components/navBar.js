4dd134da46efbb8ffb906aa6352243f2a74749be
Student_GA106 <student_ga106@Student-GA106.local>
2020-05-16 23:20:43 -0400

    added main buttons div

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -5,8 +5,9 @@ export class NavBar extends React.Component{
     render(){
         return (
             <div id="Charles">
+                <div id="userPic"></div>
+                <div id="mainButtons"></div>
 
-<div id="userPic"></div>
             </div>
         );
     }
