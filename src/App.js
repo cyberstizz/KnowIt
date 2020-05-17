b1e494c4cb684b3712b25eed14ace6b5f6579e4a
Briandepalmas <Student_GA110@Student-GA110.home>
2020-05-17 12:41:34 -0400

    cleanls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import Modal from './components/Modals'
-
+import Modals from './components/Modals'
 import axios from 'axios'
 import Signup from './components/Signup'
 import DOMp from './components/assets/DOMp'
@@ -9,16 +8,9 @@ export default class App extends Component {
  
   render() {
     return (
-      <div>
-        <Modal/>
-      </div>
+      
       <React.Fragment>
-        <Signup />
-        {/* <div>
-          <ul>
-            {this.state.data.map((recipe, id) => <h4 key={id}> {recipe.title} </h4>)}
-          </ul>
-        </div> */}
+        <Modals/>
         <DOMp/>
       </React.Fragment>  
     )
