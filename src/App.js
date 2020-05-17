33f250a1fd607c4fa5efa4ecbc152b3dcb38440a
Student_M7 <Student_M7@PD-DAL-M7.local>
2020-05-17 12:43:43 -0400

    4x  :  DOMp Refactored

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import Signup from './components/Signup'
-import DOMp from './components/assets/DOMp'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Signup from './components/Signup';
+import DOMp from './components/assets/DOMp';
 export default class App extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
       data: []
     }
   }
 
   componentDidMount() {
-    
-
     axios.get("https://api.spoonacular.com/recipes/search?query=cheese&number=4&apiKey=27a02bbb5b48401f96bfda6a7d3e2545")
+
     .then(res => {
       const recipes = res.data.results;
       console.log("recipes ---->", recipes)
-      
       this.setState({data: recipes})
-
     })
    
     .catch(error => {
       console.log('there is an error', error)
     })
-
   }
+
   render() {
     return (
       <React.Fragment>
         <Signup />
-        {/* <div>
-          <ul>
-            {this.state.data.map((recipe, id) => <h4 key={id}> {recipe.title} </h4>)}
-          </ul>
-        </div> */}
         <DOMp/>
       </React.Fragment>  
     )
   }
 }
 
+/* 
+<div>
+  <ul>
+    {this.state.data.map((recipe, id) => <h4 key={id}> {recipe.title} </h4>)}
+  </ul>
+</div> 
+*/
---
+++ src/components/assets/Card.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import img1 from '../img/SevenKeyElementofPMP.jpg';
+
+// My Logic here:... 
+// [1]   Get autoSearch input for apiQuerry
+// [2]   Fetch queryData From Api
+// [3]   Fetch Images Method{}
+// [4]   Fetch NoteText Method{}
+// [5]   Fetch mediaANOTATION (sound,video,data[graphy,drawings,etc.]){}
+
+
+
+export default class Class extends Component {
+    render() {
+        return (
+            <React.Fragment>
+
+                <div className = "card4note">
+                <img src={img1} alt=" A list of  Seven Key Element of a Successful Project" style={card_temp_Style}/>  {/* define Margines, boundaries, Padding & SizeConstrains in SASS */}
+                    <div className = "note4card">
+                        <p className = "cardNote" style={note_temp_style}><b>CARD TITLE:  Project Planning...    </b>        
+                         LoremIpsum: El sector educativo, con un volumen de 5 trillones de dólares, ede forma constante.    ProjectPlanning |<b>DECK</b> [Project Managment |<b>BOK</b>] </p> 
+                        {/* <h4 className = "cardTitle"> Title_Goes_Here</h4> */}
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    };
+};
+
+
+const note_temp_style ={
+    color: '#10d9',
+    background:'#ddd',
+    // width:'80%',
+    // border: '5px solid lightgrey',
+    // padding:'5px',
+    wordWrap: 'break-word',
+    marginTop: '0',
+    maxWidth: '35%',
+    marginLeft: '25%',
+    marginRight: '25%',
+    textAlign: 'center',
+    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)',
+};
+
+const card_temp_Style = {
+    display: 'block',
+    marginTop: '50px',
+    // float: 'center',
+    maxWidth: '35%',
+    height: 'auto',
+    marginLeft: '25%',
+    marginRight: '25%',
+    border: '1px solid #ddd',
+    objectFit: 'contain',
+    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)',
+    // .responsive{}
+    // @mdedia only screen and (max-width: 700px){}
+};
+
