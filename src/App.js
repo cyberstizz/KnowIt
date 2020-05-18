88800dfa6eb83a295afa0fcf9261b767062d748e
Student_M7 <Student_M7@PD-DAL-M7.local>
2020-05-17 21:46:17 -0400

    7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Modals from './components/Modals';
 import DOMp from './components/assets/DOMp';
+import Cards from './components/assets/Cards';
 
 export default class App extends Component {
 
