0d45c1ee3fbea8998706ef5238f87e0f3ce2f444
Student_M7 <Student_M7@PD-DAL-M7.local>
2020-05-18 19:43:25 -0400

    Splah Page Refactored

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -7,28 +7,22 @@ import LoginReqs from './LoginReqs'
 class Splash extends React.Component{
 
   render(){
-  return (
-<div id="splashBody">
-<div id="logo"></div>
-<div id="navItems">
-    
-   
-    <ul>
-        <li className="navlist">Signup</li>
-        <li className="navlist">About</li>
-        <li className="navlist">Help</li>
+    return (
+      <React.Fragment>
+        <div id="logo"></div>
+        <div id="splashBody">
+        <div id="navItems">
+          <ul>
+              <li className="navlist">Signup</li>
+              <li className="navlist">About</li>
+              <li className="navlist">Help</li>
 
-    </ul>
-     
-   
-</div>
-<div id="centerText"><h1>Remember everything you learn <br/><span id="emphasis">From now on!</span></h1><Modals/></div>
-
-
-    <UserProvider>
-     <LoginReqs/>
-    </UserProvider> 
-</div>  
+          </ul>
+        </div>
+        <div id="centerText"><h1>Remember everything you learn <br/><span id="emphasis">From now on!</span></h1><Modals/></div>
+        <UserProvider/>
+        </div>  
+      </React.Fragment>
 );
   }
 }
