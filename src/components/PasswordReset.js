efe575fc8164fa7ae067722b59ba510885a13fdd
Briandepalmas <Student_GA110@Student-GA110.home>
2020-05-17 21:54:56 -0400

    cleaned components -B

--- src/components/PasswordReset.js
+++ src/components/PasswordReset.js
@@ -32,23 +32,23 @@ const PasswordReset = () => {
  
  
   return (
-    <div className="mt-8">
-      <h1 className="text-xl text-center font-bold mb-3">
+    <div>
+      <h1>
         Reset your Password
       </h1>
-      <div className="border border-blue-300 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
+      <div>
         <form action="">
           {emailHasBeenSent && (
-            <div className="py-3 bg-green-400 w-full text-white text-center mb-3">
+            <div>
               An email has been sent to you!
             </div>
           )}
           {error !== null && (
-            <div className="py-3 bg-red-600 w-full text-white text-center mb-3">
+            <div>
               {error}
             </div>
           )}
-          <label htmlFor="userEmail" className="w-full block">
+          <label htmlFor="userEmail">
             Email:
           </label>
           <input
