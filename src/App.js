7abdb3a3366c5dd561c972de7f30a4671a3ad722
Student_M7 <Student_M7@PD-DAL-M7.local>
2020-05-18 19:06:51 -0400

    7:06pm BackUp Branch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Splash from './components/Splash'
-
+// import DOMp from './components/assets/DOMp'
 
 function App(){
 
-
+  // Splash
     return (
       <React.Fragment>
         <div>
-          <Splash/>
+          <Splash/>      
         </div>
       </React.Fragment> 
 
