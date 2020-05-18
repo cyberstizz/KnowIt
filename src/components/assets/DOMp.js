2714e4070aec157b96b7c4e1cffbd339ea2bdb52
Student_M7 <Student_M7@PD-DAL-M7.local>
2020-05-18 19:03:26 -0400

    7pm BackUp

--- src/components/assets/DOMp.js
+++ src/components/assets/DOMp.js
@@ -5,18 +5,18 @@ import '../styles/DOMp.css';
 export default class DOMp extends Component {
     render() {
         return (
-            <React.Fragment >
-
-                <div className = "Header">
+            <React.Fragment>
+            <div className = "grid ">
+                {/* <div className = "Header">
                     <div className = "NavBar">
                         <p className ="bx"> <b >KnoWIT</b></p>                            
                         <p className ="headR"> HowIt Works   |   Search  |   Help   |   AboutUs    [ SignIn | SignUp ]</p>
                         {/* <p style={bx}> <b >KnoWIT</b></p>                            
-                        <p style={headR}>HowIt Works   |   Search  |   Help   |   AboutUs    [ SignIn | SignUp ]</p> */}
+                        <p style={headR}>HowIt Works   |   Search  |   Help   |   AboutUs    [ SignIn | SignUp ]</p> 
                     </div>
-                </div>
+                </div> */}
 
-                <div className = "SideBar">
+                <div className = "Box SideBar">
                     <ul> DASHBOARD
                         <li>       </li>
                         <li>STUDY</li>
@@ -36,12 +36,12 @@ export default class DOMp extends Component {
                     </ul>
                 </div>
 
-                <div className = "mainDOMp">
+                <div className = "Box mainDOMp">
                     <Card/>
                 </div>
 
                 
-
+            </div>
             </React.Fragment>
         );
     };
