120894eba92d487877a17b32ff61d7c3be428b48
Student_M7 <Student_M7@PD-DAL-M7.local>
2020-05-18 14:09:26 -0400

    10.0

--- src/components/assets/DOMp.js
+++ src/components/assets/DOMp.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import '../styles/DOMp.css';
 
 export default class DOMp extends Component {
     render() {
         return (
             <React.Fragment >
-                <div style={styleDOMp}>
+
+                <div className = "Header">
+                    <div className = "NavBar">
+                        <p className ="bx"> <b >KnoWIT</b></p>                            
+                        <p className ="headR"> HowIt Works   |   Search  |   Help   |   AboutUs    [ SignIn | SignUp ]</p>
+                        {/* <p style={bx}> <b >KnoWIT</b></p>                            
+                        <p style={headR}>HowIt Works   |   Search  |   Help   |   AboutUs    [ SignIn | SignUp ]</p> */}
+                    </div>
+                </div>
+
+                <div className = "SideBar">
+                    <ul> DASHBOARD
+                        <li>       </li>
+                        <li>STUDY</li>
+                        <li>       </li>
+                        <li>CREATE</li>
+                        <li>       </li>
+                        <li>EDIT</li>
+                        <li>       </li>
+                        <li>DELETE</li>
+                        <li>       </li>
+                        <li>SHARE</li>
+                        <li>       </li>
+                        <li>SETTINGS</li>
+                        <li>       </li>>
+                        <li>SIGNOUT</li>
+                        <li>       </li>
+                    </ul>
+                </div>
+
+                <div className = "mainDOMp">
                     <Card/>
                 </div>
+
+                
+
             </React.Fragment>
         );
     };
 };
 
-const styleDOMp = {
-backgroundColor : 'lightgreen',
-
-}
