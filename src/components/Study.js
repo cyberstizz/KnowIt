11f53eb30f4cd5d8b266b0f69c8bb876f40e7c97
Student_GA106 <student_ga106@Student-GA106.local>
2020-05-19 00:26:52 -0400

    added api call to the party

--- src/components/Study.js
+++ src/components/Study.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import './App.css';
-import Charles from './Charle'
+import Charles from './Charle';
+import axios from 'axios';
 
 
 
  class Study extends React.Component{
+   constructor(){
+     super()
+     this.state = {
+       inspiration: ''
+     }
+   }
+   componentDidMount(){
+  axios.get("https://type.fit/api/quotes")
+  .then(response => {
+    let quote = response.data;
+    this.setState({
+      inspiration: quote[3]
+    })
 
+  })
+}
   render(){
+    
   return (
     <div id="navAndStudy">
         <Charles/>
           <div id="studyPage">
           <div id="logoDiv"></div>
           <h1 id="studyWelcome">Welcome user</h1>
-          <div id="apiQuote">api quote></div>
+          <div id="apiQuote">{this.state.inspiration.text}<br/>-{this.state.inspiration.author}</div>
           <div id="studyCount">you have blank decks</div>
           <form id="studyPageForm" onSubmit={this.getDecks}>
           <input id="studySearch" type="text" placeholder="Your shows"></input>
