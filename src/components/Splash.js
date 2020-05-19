5abdd968e54cbb8d058df3ca8a7faff47060a536
Briandepalmas <Student_GA110@Student-GA110.home>
2020-05-18 21:23:31 -0400

    router

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,8 +1,11 @@
 import React from 'react';
- import '../App.css'
+import {Route,Switch, Link , BrowserRouter as Router} from 'react-router-dom';
+import '../App.css'
 import Modals from "./Modals";
 import UserProvider from '../UserProvider'
 import LoginReqs from './LoginReqs'
+import SignUp from './SignUp'
+import PasswordReset from './PasswordReset';
  
 class Splash extends React.Component{
 
@@ -12,15 +15,20 @@ class Splash extends React.Component{
 <div id="logo"></div>
 <div id="navItems">
     
-   
+   <Router>
     <ul>
-        <li className="navlist">Signup</li>
+        <li className="navlist">
+            <Link to ="/signUp">Signup</Link>
+        </li>
         <li className="navlist">About</li>
         <li className="navlist">Help</li>
-
     </ul>
-     
    
+    <Switch>
+          <Route exact path="/signUp" component={SignUp} />
+          <Route exact path="/passwordReset" component={PasswordReset} />
+     </Switch>
+   </Router>
 </div>
 <div id="centerText"><h1>Remember everything you learn <br/><span id="emphasis">From now on!</span></h1><Modals/></div>
 
